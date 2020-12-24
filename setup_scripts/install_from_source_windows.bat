395be1403ff4cbac9543f31e08260907773f6b29
Mayank Jain <mayank10.j@gmail.com>
2020-12-24 20:57:10 +0530

    updated installation scripts for windows

--- setup_scripts/install_from_source_windows.bat
+++ setup_scripts/install_from_source_windows.bat
@@ -1,5 +1,5 @@
 cd ..
-pip install torch==1.7.0+cpu torchvision==0.8.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.5.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 python setup.py install
 pushd .
 if not exist %userprofile%\.alternat mkdir %userprofile%\.alternat
