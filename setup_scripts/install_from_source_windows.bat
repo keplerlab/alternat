77ac9167374112b5072db7015989face8c158fb6
Mayank Jain <mayank10.j@gmail.com>
2021-01-07 21:10:30 +0530

    fixed bug in installation windows script

--- setup_scripts/install_from_source_windows.bat
+++ setup_scripts/install_from_source_windows.bat
@@ -1,5 +1,5 @@
 cd ..
-pip install torch==1.5.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.4.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 python setup.py install
 pushd .
 if not exist %userprofile%\.alternat mkdir %userprofile%\.alternat
